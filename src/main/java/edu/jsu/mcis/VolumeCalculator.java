5274212fe9b6108f718c10db3991360f56dcddad
Scott Troutman <troutmansm@gmail.com>
2015-02-05 16:32:25 -0600

    Set up java files, edit build.gradle and readme.md

---
+++ src/main/java/edu/jsu/mcis/VolumeCalculator.java
@@ -0,0 +1,5 @@
+package edu.jsu.mcis;
+
+public class VolumeCalculator{
+    
+}
---
+++ src/test/java/edu/jsu/mcis/VolumeCalculatorTest.java
@@ -0,0 +1,8 @@
+package edu.jsu.mcis;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class VolumeCalculatorTest{
+    
+}
