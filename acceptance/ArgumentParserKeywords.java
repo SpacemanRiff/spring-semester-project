b59a70b042134b67c969bdac6ca5b250e0442406
btaylor4 <btaylor4@jsu.edu>
2015-02-06 13:11:36 -0500

    Added First Keywords
    startProggramWithArguments, getLength, getWidth, getHeight

--- acceptance/ArgumentParserKeywords.java
+++ acceptance/ArgumentParserKeywords.java
@@ -1,1 +1,21 @@
-test
+import edu.jsu.mcis.*;
+
+public class ArgumentParserKeywords{
+    ArgumentParser a;
+    String arguments;
+    
+    public void startProgramWithArguments(String args){
+        a = new ArgumentParser();
+        arguments = args;
+        a.sendArguments(arguments);
+    }
+    public float getLength(){
+        a.getArgumentAt(0);
+    }
+    public float getWidth(){
+        a.getArgumentAt(1);
+    }
+    public float getHeight(){
+        a.getArgumentAt(2);
+    }
+}
