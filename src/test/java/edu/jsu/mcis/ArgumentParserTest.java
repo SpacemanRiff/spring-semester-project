984c493a7f23905ebb5c290d663d72ec1d874681
Scott Troutman <troutmansm@gmail.com>
2015-02-06 11:23:52 -0600

    Created Unit Tests

--- src/test/java/edu/jsu/mcis/ArgumentParserTest.java
+++ src/test/java/edu/jsu/mcis/ArgumentParserTest.java
@@ -5,7 +5,30 @@ import static org.junit.Assert.*;
 
 public class ArgumentParserTest{
     @Test
-    public void testGetArguments(){
+    public void testAddArguments(){
         ArgumentParser a = new ArgumentParser();
+        String[] arguments = new String[3];
+        
+        arguments[0] = "1";
+        arguments[1] = "4";
+        arguments[2] = "7";
+        a.addArguments(arguments);
+        
+        for(int i = 0; i < arguments.size(), i++){
+            assertEquals(arguments[i], a.getArguments(i));
+        }
+    }
+    
+    @Test
+    public void testCalculateVolume(){
+        ArgumentParser a = new ArgumentParser();
+        String[] arguments = new String[3];
+        
+        arguments[0] = "1";
+        arguments[1] = "4";
+        arguments[2] = "7";
+        a.addArguments(arguments);
+
+        assertEquals(arguments[0]*arguments[1]*arguments[2], a.calcVolume);
     }
 }
