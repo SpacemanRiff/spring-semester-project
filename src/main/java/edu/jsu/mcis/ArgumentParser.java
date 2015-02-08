1c79147db3f519d201ae165563fc60802abdc69d
Scott Troutman <troutmansm@gmail.com>
2015-02-07 20:11:57 -0600

    added a comment to the constructor
    hopefully this will give us a bit of guidance on Monday or whenever
    someone edits this code next.

--- src/main/java/edu/jsu/mcis/ArgumentParser.java
+++ src/main/java/edu/jsu/mcis/ArgumentParser.java
@@ -4,23 +4,25 @@ public class ArgumentParser
 {
 	private String mainInput;
 	
-	public ArgumentParser()
-	{
-    
+	public ArgumentParser(){
+        /*thinking we should have the constructor take in an array of strings
+        * that are the names of the arguments he wants to take in, which
+        * in turn will also tell us the number of integer/float/double valued
+        * arguments to take in (this is not including flags like --type sphere or whatever)
+        *
+        * for instance, {"length","width","height"}
+        */
     }
 	
-	public String getArguments()
-	{
+	public String getArguments(){
 		return mainInput;
 	}
 	
-	public void sendArguments(String arguments)
-	{
+	public void sendArguments(String arguments){
 		String mainInput = arguments;
 	}
     
-    public static void main(String[] args)
-	{
+    public static void main(String[] args){
 	
     }
     
