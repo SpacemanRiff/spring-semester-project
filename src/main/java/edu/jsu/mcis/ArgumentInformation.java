9c0bcb9a1fc81478fda4941edd3d274abad3bdad
Scott Troutman <troutmansm@gmail.com>
2015-02-20 12:02:39 -0600

    separated ArgumentInformation to its own file

---
+++ src/main/java/edu/jsu/mcis/ArgumentInformation.java
@@ -0,0 +1,65 @@
+package edu.jsu.mcis;
+
+import edu.jsu.mcis.ArgumentParser.Types;
+
+public class ArgumentInformation{
+    private String description;
+    private Types type;
+    private Object value;
+    private int position;
+    
+    public ArgumentInformation(String description, Types type){
+        this.description = description;
+        this.type = type;
+    }
+    
+    public void setPosition(int i){
+        position = i;
+    }
+    
+    public void setValue(String value){
+        switch(type){
+            case INTEGER:
+                this.value = Integer.parseInt(value);
+                break;
+            case FLOAT:
+                this.value = Float.parseFloat(value);
+                break;
+            case BOOLEAN:
+                this.value = Boolean.parseBoolean(value);
+                break;
+            default:
+                this.value = value;
+                break;
+        }
+    }
+    
+    public Object getValue(){
+        return value;
+    }
+    
+    public int getPosition(){
+        return position;
+    }
+    
+    public String getDescription(){
+        return description;
+    }
+    
+    public Types getType(){
+        return type;
+    }
+    
+    public String getTypeAsString(){            
+        switch(type){
+            case INTEGER:
+                return "INTEGER";
+            case FLOAT:
+                return "FLOAT";
+            case BOOLEAN:
+                return "BOOLEAN";
+            default:
+                return "STRING";
+        }
+    }
+}
