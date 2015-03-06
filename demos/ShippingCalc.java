638463b8edfba2434bef2d88cde3d8a805bc63fa
cingram1 <cingram1@jsu.edu>
2015-03-05 20:54:45 -0600

    Added the shipping demo
    also included a .bat file

---
+++ demos/ShippingCalc.java
@@ -0,0 +1,38 @@
+import edu.jsu.mcis.*;
+
+public class ShippingCalc{		
+	public static void main(String[] args){
+		
+		ArgumentParser p = new ArgumentParser();
+		
+		p.setProgramDescription("Shipping Order");
+		
+		p.addArgument("Item 1", "The item you want to ship", ArgumentParser.Types.STRING);
+		p.addArgument("Price 1", "The price of the first item", ArgumentParser.Types.FLOAT);
+		p.addArgument("Shipping Cost", "The cost to ship this item", ArgumentParser.Types.FLOAT);
+		p.addOptionalArgument("quantity", "Allows you to order multiple of the same item", ArgumentParser.Types.INTEGER, 1);
+		p.addOptionalArgument("weight", "The weight of the item", ArgumentParser.Types.STRING, "n/a");
+        p.addOptionalFlag("local", "This item will be shipped locally");
+		
+		p.parse(args);
+        
+        boolean local = p.getOptionalArgumentValueOf("local");
+        
+		System.out.println("\n\nYou have requested to ship "  + p.getOptionalArgumentValueOf("quantity")
+                            + " " + p.getValueOf("Item 1") + "(s)\nEach " + p.getValueOf("Item 1") + " costs $"
+                            + p.getValueOf("Price 1") + "\nShipping cost is $" + p.getValueOf("Shipping Cost") 
+							+ "\nThe weight of this item is: " + p.getOptionalArgumentValueOf("weight") + "lbs");
+		
+		float shipping = p.getValueOf("Shipping Cost");
+		float price = p.getValueOf("Price 1");
+		float total = shipping + price;
+		
+		System.out.println("\n\nTotal cost: $" + total);
+		
+        if(local){
+            System.out.println("This will be shipped locally");
+        }else{
+            System.out.println("This will be shipped internationally");
+        }
+	}
+}
---
+++ demos/runShippingCalcDemo.bat
@@ -0,0 +1,3 @@
+javac -cp .;..\build\classes\main ShippingCalc.java
+java -cp .;..\build\classes\main ShippingCalc Laptop --weight 10 150.35 15.54 --local --quantity 3
+pause
