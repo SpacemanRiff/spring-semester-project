5bacec764c3889d5e26f3d6356c4ba73f867f070
cingram1 <cingram1@jsu.edu>
2015-03-12 22:37:32 -0600

    added xml demo
    made a pizzaorder demo that reads from an xml file

---
+++ XMLPizzaDemo/PizzaOrderFromXML.java
@@ -0,0 +1,20 @@
+import edu.jsu.mcis.*;
+
+public class PizzaOrderFromXML{		
+    public static void main(String[] args){
+        ArgumentParser p = new ArgumentParser();
+        XMLManager manager = new XMLManager();
+		
+        p.setProgramDescription("Pizza Creator");
+        
+        manager.loadArguments("PizzaArguments.xml", p); 
+		
+        p.parse(args);
+        
+		System.out.println("\nYou have ordered "  + p.getOptionalArgumentValueOf("quantity")
+                            + " " + p.getValueOf("size") + " pizza(s) with " + p.getValueOf("crust")
+                            + " crust and " + p.getValueOf("toppings") + " topping(s)");
+		System.out.println("Drink: " + p.getOptionalArgumentValueOf("drink"));
+	}
+}
+
