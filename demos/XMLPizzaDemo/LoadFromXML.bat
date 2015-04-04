2750dc7cdbf63b7ac7acfa24520c6efb095252bf
cingram1 <cingram1@jsu.edu>
2015-04-04 15:21:14 -0600

    XMLdemo update

--- demos/XMLPizzaDemo/LoadFromXML.bat
+++ demos/XMLPizzaDemo/LoadFromXML.bat
@@ -1,3 +1,3 @@
 javac -cp .;..\..\build\classes\main PizzaOrderFromXML.java
-java -cp .;..\..\build\classes\main PizzaOrderFromXML Large Hand-tossed 5 --quantity 3 --drink Coke
+java -cp .;..\..\build\classes\main PizzaOrderFromXML Large thick 2 --quantity 3 --drink Coke
 pause
--- demos/XMLPizzaDemo/WritePizzaArgs.java
+++ demos/XMLPizzaDemo/WritePizzaArgs.java
@@ -5,12 +5,15 @@ public class WritePizzaArgs{
 		
 		ArgumentParser p = new ArgumentParser();
 		
-		p.setProgramDescription("Pizza Creator");
+		Object[] restrictedSizes = {"small", "Small", "medium", "Medium", "large", "Large"};
+        
+        p.setProgramDescription("Pizza Creator");
 		
 		p.addPositionalArgument("size", "The size of the pizza (Small, Medium, or Large)", ArgumentParser.Types.STRING);
-		p.addPositionalArgument("crust", "The style of pizza crust", ArgumentParser.Types.STRING);
+		p.setRestrictedValues("size", restrictedSizes);
+        p.addPositionalArgument("crust", "The style of pizza crust", ArgumentParser.Types.STRING);
 		p.addPositionalArgument("toppings", "The number of toppings for the pizza", ArgumentParser.Types.INTEGER);
-		p.addNamedArgument("quantity", "Allows you to order multiple of the same pizza", ArgumentParser.Types.INTEGER, 1);
+		p.addRequiredNamedArgument("quantity", "Allows you to order multiple of the same pizza", ArgumentParser.Types.INTEGER, 1);
 		p.addNamedArgument("drink", "The drink you will have with your pizza", ArgumentParser.Types.STRING, "No Drink");
 		
 		p.parse(args);
